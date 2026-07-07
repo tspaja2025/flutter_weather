fc3f5d728fc39a8ee64514d0f97b51b2e34e0894
tspaja2025 <tspaja2025@outlook.com>
2026-07-07 09:00:14 +0300

    Update cities_screen.dart

--- lib/screens/cities_screen.dart
+++ lib/screens/cities_screen.dart
@@ -9,7 +9,14 @@ class CitiesScreen extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Manage Cities'),
+        Text(
+          'Manage Cities',
+          style: Theme.of(context).textTheme.displayLarge?.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+        const SizedBox(height: 16),
+
         SizedBox(
           width: double.infinity,
           child: TextField(
@@ -29,108 +36,44 @@ class CitiesScreen extends StatelessWidget {
           ],
         ),
 
-        Card(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(18),
-            side: BorderSide(
-              color: Theme.of(context).colorScheme.outlineVariant,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('New York'),
-                    const Text('09:42 AM'),
-                    const Text('Cloudy'),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('18°'),
-                    Row(children: [const Text('H: 21°'), const Text('L: 14°')]),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        CityCard(
+          city: 'New York',
+          time: '09:42 AM',
+          weather: 'Cloudy',
+          weatherIcon: Symbols.cloud,
+          temperature: 18,
+          high: 21,
+          low: 14,
         ),
 
-        Card(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(18),
-            side: BorderSide(
-              color: Theme.of(context).colorScheme.outlineVariant,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('London'),
-                    const Text('02:42 PM'),
-                    const Text('Light Rain'),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('12°'),
-                    Row(children: [const Text('H: 28°'), const Text('L: 8°')]),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        CityCard(
+          city: 'London',
+          time: '02:42 PM',
+          weather: 'Light Rain',
+          weatherIcon: Symbols.rainy,
+          temperature: 12,
+          high: 15,
+          low: 11,
         ),
 
-        Card(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(18),
-            side: BorderSide(
-              color: Theme.of(context).colorScheme.outlineVariant,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Tokyo'),
-                    const Text('10:42 AM'),
-                    const Text('Clear'),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('24°'),
-                    Row(children: [const Text('H: 28°'), const Text('L: 19°')]),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        CityCard(
+          city: 'Tokyo',
+          time: '10:42 PM',
+          weather: 'Clear',
+          weatherIcon: Symbols.sunny,
+          temperature: 24,
+          high: 28,
+          low: 19,
         ),
+
+        const SizedBox(height: 16),
 
         const Text('RECOMMENDED CITIES'),
 
+        const SizedBox(height: 16),
+
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             OutlinedButton(onPressed: () {}, child: const Text('Paris')),
             OutlinedButton(onPressed: () {}, child: const Text('Berlin')),
@@ -139,6 +82,107 @@ class CitiesScreen extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+}
+
+class CityCard extends StatelessWidget {
+  final String city;
+  final String time;
+  final String weather;
+  final IconData weatherIcon;
+  final double temperature;
+  final double high;
+  final double low;
+
+  const CityCard({
+    super.key,
+    required this.city,
+    required this.time,
+    required this.weather,
+    required this.weatherIcon,
+    required this.temperature,
+    required this.high,
+    required this.low,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(18),
+        side: BorderSide(color: Theme.of(context).colorScheme.outlineVariant),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  city,
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(time, style: Theme.of(context).textTheme.titleSmall),
+                const SizedBox(height: 4),
+                Text(
+                  weather,
+                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 180,
+            height: 120,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: -30,
+                  right: -20,
+                  child: Icon(
+                    weatherIcon,
+                    fill: 1,
+                    size: 120,
+                    color: Theme.of(
+                      context,
+                    ).colorScheme.primary.withValues(alpha: .10),
+                  ),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 18,
+                  child: Text(
+                    '$temperature°',
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 16,
+                  bottom: 16,
+                  child: Row(
+                    children: [
+                      Text('H: $high°'),
+                      const SizedBox(width: 8),
+                      Text('L: $low°'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
